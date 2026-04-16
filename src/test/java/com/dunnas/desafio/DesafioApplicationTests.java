7d0e26bd505133d0bd4320a84820c976fa22ac29
robertfabio <fabio.filho25453@alunos.ufersa.edu.br>
2026-04-16 15:41:52 -0300

    first commit

---
+++ src/test/java/com/dunnas/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dunnas.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
